5cdcfe550e4c3b067dc36d976e99d35633f0c3fc
bach <67271736+Bachnd23@users.noreply.github.com>
2024-06-28 19:40:30 +0700

    update invoice

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -19,6 +19,10 @@ namespace COCOApp.Controllers
         [HttpPost]
         public IActionResult CreateSummary(List<int> orderIds)
         {
+            if(orderIds==null || orderIds.Count <= 0)
+            {
+                return View("/Views/Report/CreateReport.cshtml");
+            }
             // Assuming _orderService can fetch orders by their IDs
             List<Order> orders = _orderService.GetOrdersByIds(orderIds);
 
@@ -29,13 +33,18 @@ namespace COCOApp.Controllers
         public IActionResult CreateInvoice(List<int> orderIds, List<decimal> costs)
         {
             List<Order> orders = _orderService.GetOrdersByIds(orderIds);
-            ViewBag.OrderCosts = costs;
+            if(orders==null|| orders.Count == 0) {
+                return View("/Views/Report/CreateReport.cshtml");
+            }
+            decimal ordersTotalCost = 0;
             for (int i=0;i<orders.Count;i++)
             {
                 Order order = orders[i];
                 order.OrderProductCost = costs[i];
+                order.OrderTotal= costs[i]*order.Volume;
+                ordersTotalCost+= order.OrderTotal;
             }
-
+            ViewBag.totalCost=ordersTotalCost;
             // Pass orders to the view
             return View("/Views/Report/Invoice.cshtml", orders);
         }
